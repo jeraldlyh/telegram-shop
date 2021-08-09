745f63a7f3b1d622db394e05cfee8819849d9164
Jerald <jeraldssw@gmail.com>
2021-08-09 17:31:55 +0800

    Clear message in root app page upon changing scenes

--- telegram/app.js
+++ telegram/app.js
@@ -2,6 +2,10 @@ const { Telegraf, session, Scenes } = require("telegraf")
 const CustomScenes = require("./scenes")
 const db = require("../database")
 const Dummy = require("./commands/dummy")
+const Models = require("../database/models")
+const Database = require("../database/actions")
+const Utils = require("./utils")
+const faker = require("faker")
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
@@ -26,15 +30,36 @@ bot.use(stage.middleware())
 
 // Commands
 bot.command("start", async (ctx) => {
-    await ctx.deleteMessage()
+    ctx.deleteMessage()
+    Utils.clearScene(ctx, false)
     ctx.scene.enter("WELCOME_SCENE")
+})
+
+bot.command("test", ctx => Dummy.createDummyData(ctx))
+bot.command("voucher", async (ctx) => {
+    const shop = await Database.getShopByID(ctx.botInfo.id)
+
+    const voucher = await Models.Voucher.create({
+        code: faker.lorem.word(),
+        discount: Math.floor(Math.random() * (20 - 1 + 1) + 1),     // 1% - 20%
+        shopID: shop.toJSON().botID,
+        isValid: true,
+    })
+    ctx.reply(voucher.code)
+})
+
+bot.on("message", ctx => {
+    console.log(ctx.message.message_id)
+    if (ctx.session.cleanUpState && ctx.session.cleanUpState.length > 0) {
+        Utils.updateCleanUpState(ctx, ctx.message.message_id)
+    } else {
+        ctx.session.cleanUpState = [ctx.message.message_id]
+    }
 })
 
 bot.on("pre_checkout_query", async (ctx) => {
     await ctx.answerPreCheckoutQuery(true)
 })
-
-bot.command("test", ctx => Dummy.createDummyData(ctx))
 
 bot.launch({ dropPendingUpdates: true })
 process.once('SIGINT', () => bot.stop('SIGINT'))
