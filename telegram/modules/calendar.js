8447ee93329a8312cdc62f04d17ebcf9ce2152f5
Jerald <jeraldssw@gmail.com>
2021-08-11 23:27:16 +0800

    Refactor code for calendar function

--- telegram/modules/calendar.js
+++ telegram/modules/calendar.js
@@ -13,19 +13,10 @@ module.exports = {
         return await ctx.replyWithHTML(Template.dateConfirmationMessage(date), Template.confirmationButtons())
     },
     editMessageByID: async function (ctx, messageID, month) {
-        console.log(month)
         return await ctx.telegram.editMessageText(ctx.chat.id, messageID, undefined, Template.calendarMessage(), module.exports.calendarButtons(month - 1))
     },
     getCalendar: function (month) {
-        var now = moment()
-        if (month) {
-            const difference = month - now.month()
-            if (difference < 0) {
-                now = now.subtract(Math.abs(difference), "months")
-            } else {
-                now = now.add(difference, "months")
-            }
-        }
+        const now = month ? moment().set("month", month) : moment()
 
         var calendar = [[{
             text: `${now.format("MMMM")} ${now.year()}`,
