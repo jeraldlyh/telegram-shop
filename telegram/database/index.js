3e9bb228eba517f6bccf2c3fadada564077db810
Jerald <jeraldssw@gmail.com>
2021-12-05 16:45:08 +0800

    Temporarily disable dialect options

--- telegram/database/index.js
+++ telegram/database/index.js
@@ -2,13 +2,16 @@ const { Sequelize } = require("sequelize")
 
 
 const sequelize = new Sequelize(
-    process.env.PG_DATABASE,
-    process.env.PG_USER,
-    process.env.PG_PASSWORD,
+    process.env.DATABASE_URL,
     {
-        host: process.env.PG_HOST,
         dialect: "postgres",
-        logging: false,
+        protocol: "postgres",
+        // dialectOptions: {
+        //     ssl: {
+        //         require: true,
+        //         rejectUnauthorized: false
+        //     }
+        // }
     }
 )
 
