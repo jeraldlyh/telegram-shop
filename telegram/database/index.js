5a33d7c456176663a2fbeea2c943cb2533afa570
Jerald <jeraldssw@gmail.com>
2021-12-05 17:01:58 +0800

    Add force update to DB

--- telegram/database/index.js
+++ telegram/database/index.js
@@ -17,7 +17,10 @@ const sequelize = new Sequelize(
 
 sequelize
     .authenticate()
-    .then(() => console.log("Connection has been established successfully."))
+    .then(() => {
+        console.log("Connection has been established successfully.")
+        sequelize.sync({ force: true })
+    })
     .catch(error => console.log("Unable to connect to the database", error))
 
 module.exports = sequelize
